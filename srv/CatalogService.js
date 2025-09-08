9907638bc2a3dc0c5284600cc78c674a7d17861f
Julian-Kainz <julian.kainz@clouddna.at>
2025-09-08 13:44:16 +0200

    test: add test script and unit tests for addCourse function

--- srv/CatalogService.js
+++ srv/CatalogService.js
@@ -2,14 +2,12 @@ const cds = require("@sap/cds");
 
 class CatalogService extends cds.ApplicationService {
   async init() {
-    console.log("Service Init");
-    const northwind = await cds.connect.to("Northwind");
-
-    this.on("READ", ["Products"], (req) => {
-      return northwind.run(req.query);
-    });
-
-    return super.init();
+    // console.log("Service Init");
+    // const northwind = await cds.connect.to("Northwind");
+    // this.on("READ", ["Products"], (req) => {
+    //   return northwind.run(req.query);
+    // });
+    // return super.init();
   }
 }
 
